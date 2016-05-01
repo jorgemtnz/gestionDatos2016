6aae706d04bb9c34409d3e7c93d8b2d542d5fc37
danielchungara89 <danielchungara89@gmail.com>
2016-05-01 20:15:51 +0100

    creacion de las pirmeras tablas

--- 002.Desarrollo/Migracion BD/Migracion.sql
+++ 002.Desarrollo/Migracion BD/Migracion.sql
@@ -7,8 +7,56 @@
 	5- Migración de datos
 */
 
-USE [GD1C2016]
+USE GD1C2016 -- Indica la base de datos a utilizar
 GO
+
+/* CREACIÓN DEL ESQUEMA */
+CREATE SCHEMA GRUPO AUTHORIZATION gd 
+GO
+
+/****************************************************** CREACIÓN DE TABLAS ******************************************************/
+
+/* TABLA ROL */
+CREATE TABLE [GRUPO].ROL (
+	[Rol_Id] numeric(5,0) NOT NULL IDENTITY(1,1),
+	[Rol_Nombre] [varchar](255) NOT NULL,
+	[Rol_Estado] char NOT NULL,
+	PRIMARY KEY(
+		[Rol_Id]
+	)
+)
+
+/* TABLA FUNCIONALIDAD */
+CREATE TABLE [GRUPO].FUNCIONALIDAD (
+	[Funcionalidad_Id] numeric(5,0) NOT NULL IDENTITY(1,1),
+	[Funcionalidad_Nombre] [varchar](255) NOT NULL,
+    PRIMARY KEY(
+		[Funcionalidad_Id]
+	)
+)
+
+/* TABLA ROL_FUNCIONALIDAD */
+CREATE TABLE [GRUPO].ROL_FUNCIONALIDAD (
+	[Rol_Id] numeric(5,0) NOT NULL FOREIGN KEY REFERENCES GRUPO.ROL(Rol_Id),
+	[Funcionalidad_Id] numeric(5,0) NOT NULL FOREIGN KEY REFERENCES GRUPO.FUNCIONALIDAD(Funcionalidad_Id),
+	PRIMARY KEY(
+		[Rol_Id],
+		[Funcionalidad_Id]
+	)
+)
+
+/* TABLA USUARIO */
+CREATE TABLE [GRUPO].USUARIO (
+	[Username] [varchar](255) NOT NULL,
+	[Password] [varchar](255) NOT NULL,
+	[Usu_Fec_Creacion] datetime NOT NULL,
+    [Usu_Intentos_Fallidos] numeric(5,0) NULL DEFAULT 0,
+	[Usu_Habilitado] char NOT NULL,
+	[Usu_Cant_Pulibaciones] int,
+    PRIMARY KEY(
+		[Username]
+	)
+)
 
 /****** Object:  Table [dbo].[Rubros]    Script Date: 24/04/2016 13:59:36 ******/
 SET ANSI_NULLS ON
